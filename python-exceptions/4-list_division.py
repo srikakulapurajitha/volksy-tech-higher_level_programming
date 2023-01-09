e1b900659c9a281372f7799ca5d92ca09a98f509
srikakulapurajitha <rajithas@brightcomgroup.com>
2023-01-09 10:10:07 -0800

    4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    c = 0
+    for i in range(list_length):
+        try:
+            c = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            c = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            c = 0
+        except IndexError:
+            print("out of range")
+            c = 0
+        finally:
+            new_list.append(c)
+    return new_list
