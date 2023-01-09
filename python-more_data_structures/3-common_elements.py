3c0d80704ca95320d006adfb9f1bb9c477f47c39
srikakulapurajitha <rajithas@brightcomgroup.com>
2023-01-08 20:46:20 -0800

    3

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    lst = []
-    for i in set_1:
-        if i in set_2:
-            lst.append(i)
-            return lst
+    return (set_1 & set_2)
