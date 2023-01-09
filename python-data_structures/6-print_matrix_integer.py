ed5bc347e8f8ab23cca0cb48c72316788c913db6
srikakulapurajitha <rajithas@brightcomgroup.com>
2023-01-08 23:17:56 -0800

    checkinngggg

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 0:
-        print()
-    else:
-        for i in range(len(matrix)):
-            for j in matrix[i]:
-                if j == matrix[i][-1]:
-                    print(j)
-                else:
-                    print("{:d}".format(j), end=" ")
+    for i in range(len(matrix)):
+        if len(matrix[i]) == 0:
+            print()
+        for j in matrix[i]:
+            if j == matrix[i][-1]:
+                print(j)
+            else:
+                print("{:d}".format(j), end=" ")
