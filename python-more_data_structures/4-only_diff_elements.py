2a61b30ca10c367432d179f16fa9c7dab8dcd62e
srikakulapurajitha <rajithas@brightcomgroup.com>
2023-01-08 20:50:20 -0800

    4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-st = []
-for i in set_1:
-    if i not in set_2:
-        lst.append(i)
-for i in set_2:
-    if i not in set_1:
-        lst.append(i)
-return lst
+def only_diff_elements(set_1, set_2):
+    lst = []
+    for i in set_1:
+        if i not in set_2:
+            lst.append(i)
+    for i in set_2:
+        if i not in set_1:
+            lst.append(i)
+    return lst
