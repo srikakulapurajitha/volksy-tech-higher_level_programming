4b9c1948446f19f361c7b4ba5894d743d7d6bdfa
srikakulapurajitha <rajithas@brightcomgroup.com>
2023-01-10 01:56:16 -0800

    task1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 '''1111'''
+
+
 class Square:
     '''square size'''
-
-
     def __init__(self, size):
         '''size'''
-        self.__size = size
 
+        self.__size = size
