2e4e8a09fe5a14097912f8dcbb1b26e67835eb1a
srikakulapurajitha <rajithas@brightcomgroup.com>
2023-01-10 03:21:21 -0800

    2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """Square"""
 
+
 class Square:
+    """size"""
     def __init__(square, size=0):
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
         square.__size = size
 
 
