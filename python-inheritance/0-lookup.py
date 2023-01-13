8db0d9ce230016d9aca391a11a01516917560490
srikakulapurajitha <rajithas@brightcomgroup.com>
2023-01-12 23:25:10 -0800

    task

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Defines an object attribute lookup function."""
+"""This module defines the lookup function"""
 
 
 def lookup(obj):
-    """Return a list of an object's available attributes."""
-    return (dir(obj))i
+    """Returns a valid list of attributes of the object"""
+    return dir(obj)
