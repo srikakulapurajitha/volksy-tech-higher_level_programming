965bd6f2333102f1460d90893c149ce0b81c8fbb
srikakulapurajitha <rajithas@brightcomgroup.com>
2023-01-18 04:46:15 -0800

    tasks

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module for saving to json"""
+
+
+import json
+import os.path
+import sys
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+json_list = []
+
+if os.path.exists(filename):
+    json_list = load_from_json_file(filename)
+
+for index in argv[1:]:
+    json_list.append(index)
+
+save_to_json_file(json_list, filename)
