7261ee4aeeeeb165c9531571c2856207762e0077
srikakulapurajitha <rajithas@brightcomgroup.com>
2023-01-24 01:41:57 -0800

    tasks

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
+    """doc str"""
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
