85a3c797763d17f5869f08bba4e5c08c006a291d
srikakulapurajitha <rajithas@brightcomgroup.com>
2023-02-07 01:15:34 -0800

    tasks

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+"""select_states"""
+
+
 import sys
 import MySQLdb
 
