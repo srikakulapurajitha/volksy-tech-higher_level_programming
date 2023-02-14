c645e966fb84d070f53c710996da585a16f0c95c
srikakulapurajitha <rajithas@brightcomgroup.com>
2023-02-13 23:16:40 -0800

    Task14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -9,12 +9,12 @@ from model_city import City
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for row in session.query(State, City).join(City).order_by(City.id):
-        print("{:}: ({:}) {:}".format(row[0].name, row[1].id, row[1].name))
-    session.commit()
-    session.close()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for row in session.query(State, City).join(City).order_by(City.id):
+        print("{:}: ({:}) {:}".format(row[0].name, row[1].id, row[1].name))
+    session.commit()
+    session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -7,8 +7,8 @@ from model_state import Base, State
 
 
 class City(Base):
-    """class that inherits from Base"""
-    __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)   
+    """class that inherits from Base"""
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)   
