69e6ba6923720a7c295b63fb2810ef0f39baafbf
srikakulapurajitha <rajithas@brightcomgroup.com>
2023-02-13 21:55:31 -0800

    Task14

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -12,5 +12,3 @@ class City(Base):
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
-
-
