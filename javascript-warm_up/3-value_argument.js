7d7d6498d483d47fda68d7710a678b612af8752a
srikakulapurajitha <you@example.com>
2023-02-20 21:53:03 -0800

    rrrrr

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-if (process.argv.length < 3) {
-  console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
