eb75e753ac9984fc2c148fb8ca2bfd3f614cf928
srikakulapurajitha <you@example.com>
2023-02-26 22:18:51 -0800

    Task5

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// function that returns the number of occurrences in a list
+
+exports.nbOccurences = function (list, searchElement) {
+  return (list.filter(e => e === searchElement).length);
+};
+
